5f3c9820e009e5c6aa00c3e1acdab2029ed5dd8f
shalant <doug.rosenberg@gmail.com>
2022-10-11 16:51:52 -0500

    2:41:03

---
+++ BlazorECommerce/BlazorECommerce/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorECommerce.Shared;
+
+namespace BlazorECommerce.Client.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Task GetProducts();
+    }
+}
